a7f7b7bd9b134c23da2084121d9161f0fde63557
Ashyia <Riikimaru07@gmail.com>
2016-05-18 13:02:20 +0100

    Add files via upload

---
+++ myProject/js/controllers.js
@@ -0,0 +1,18 @@
+angular.module('app.controllers', [])
+  
+.controller('mainPageCtrl', function($scope) {
+
+})
+   
+.controller('caloriesCtrl', function($scope) {
+
+})
+   
+.controller('excerciseCtrl', function($scope) {
+
+})
+      
+.controller('cardioCtrl', function($scope) {
+
+})
+ 
